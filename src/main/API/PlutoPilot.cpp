5bc939cb27fe183da03eea16618d5f76c51fc668
Ashish Jaiswal <j.mechash@gmail.com>
2024-12-06 21:01:28 +0530

    Formatted

--- src/main/API/PlutoPilot.cpp
+++ src/main/API/PlutoPilot.cpp
@@ -1,47 +1,30 @@
 // Do not remove the include below
 #include "PlutoPilot.h"
 
-//The setup function is called once at Pluto's hardware startup
-void plutoInit(void)
-{
-// Add your hardware initialization code here
-
+// The setup function is called once at Pluto's hardware startup
+void plutoInit ( void ) {
+  // Add your hardware initialization code here
 }
 
 
 
-//The function is called once before plutoLoop when you activate Developer Mode
-void onLoopStart(void)
-{
-// do your one time stuffs here
-
-
-
+// The function is called once before plutoLoop when you activate Developer Mode
+void onLoopStart ( void ) {
+  // do your one time stuffs here
 }
-
 
 
 
 // The loop function is called in an endless loop
-void plutoLoop(void)
-{
+void plutoLoop ( void ) {
 
-//Add your repeated code here
-
-
+  // Add your repeated code here
 }
 
 
 
-//The function is called once after plutoLoop when you deactivate Developer Mode
-void onLoopFinish(void)
-{
+// The function is called once after plutoLoop when you deactivate Developer Mode
+void onLoopFinish ( void ) {
 
-// do your cleanup stuffs here
-
-
+  // do your cleanup stuffs here
 }
-
-
-
-
